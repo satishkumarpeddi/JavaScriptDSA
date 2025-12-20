c5d347040c059d04bd88658485487a753ccfa353
evhook <anonymous1986222@gmail.com>
2025-12-20 11:42:59 +0530

    Implement Binary Tree with insert, delete methods, height calculation, and in-order traversal

---
+++ BinaryTreeStructuralFlow/BinaryTreeHeight.js
@@ -0,0 +1,55 @@
+class BinaryTreeDemo {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+function insert(root, val) {
+  if (root == null) return new BinaryTreeDemo(val);
+  if (val < root.val) root.left = insert(root.left, val);
+  else if (val > root.val) root.right = insert(root.right, val);
+  return root;
+}
+function minValueNode(root) {
+  let temp = root;
+  while (temp.left != null) {
+    temp = temp.left;
+  }
+  return temp;
+}
+function deleteMethod(root, val) {
+  if (root == null) return null;
+  if (val < root.val) root.left = deleteMethod(root.left, val);
+  else if (val > root.val) root.right = deleteMethod(root.right, val);
+  else {
+    if (root.left == null) return root.right;
+    if (root.right == null) return root.left;
+    let temp = minValueNode(root.right);
+    root.val = temp.val;
+    root.right = deleteMethod(root.right, temp.val);
+  }
+  return root;
+}
+let getHeight = (root) => {
+  if (root == null) return -1;
+  return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+};
+let root = null;
+root = insert(root, 1);
+root = insert(root, 21);
+root = insert(root, 13);
+root = insert(root, 11);
+root = insert(root, 10);
+root = insert(root, 9);
+let list = [];
+const inorder = (root) => {
+  if (root === null) return;
+  inorder(root.left);
+  list.push(root.val);
+  inorder(root.right);
+};
+inorder(root);
+console.log("In Order Traversal Of Binary Tree =>: ", list);
+list = [];
+console.log("Height of the Binary Tree =>: ", getHeight(root));
