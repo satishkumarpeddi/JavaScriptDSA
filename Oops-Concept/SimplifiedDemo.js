f18b256060e1518ec33fdcb49357f2064015db4b
evhook <anonymous1986222@gmail.com>
2026-01-19 19:29:52 +0530

    Add Student class to demonstrate object-oriented programming concepts

---
+++ Oops-Concept/SimplifiedDemo.js
@@ -0,0 +1,22 @@
+// Object Oriented Programming In JavaScript
+
+// Class and Objects
+
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  takeExam() {
+    console.log(`${this.name} is taking an exam.`);
+  }
+  takeLeave() {
+    console.log(`${this.name} is taking leave.`);
+  }
+}
+const StudentA = new Student("Alice", 20);
+StudentA.takeExam();
+StudentA.takeLeave();
+const StudentB = new Student("Bob", 22);
+StudentB.takeExam();
+StudentB.takeLeave();
